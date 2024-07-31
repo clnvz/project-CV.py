b292ddb4bb6817d11cd126ab4f7c2beeee705ea7
clnvz <cybers.colinvelasquez@gmail.com>
2024-07-31 22:27:11 +0800

    better readability
    moved turtle attributes block of code upwards

--- Intermediate Projects/Day 18 - The Hirst Painting Project/main.py
+++ Intermediate Projects/Day 18 - The Hirst Painting Project/main.py
@@ -14,24 +14,25 @@
 #######################################################################
 from turtle import Turtle, Screen
 import random
+
 canvass = Screen()
+painter = Turtle()
+painter.shape("arrow")
+canvass.colormode(255)
+painter.penup()
 
 color_list = [
     (246, 244, 243), (235, 239, 246), (241, 246, 243), (247, 239, 242), (135, 164, 199), (223, 151, 105), (31, 44, 63),
     (200, 137, 148), (160, 61, 51), (235, 212, 93), (49, 100, 139), (138, 181, 162), (147, 64, 72), (56, 49, 47),
     (161, 32, 30), (62, 115, 100), (228, 165, 171), (51, 40, 43), (169, 29, 33), (210, 86, 76), (236, 167, 156),
     (34, 60, 54), (16, 95, 70), (34, 60, 105), (171, 188, 219), (191, 101, 109), (109, 127, 155), (174, 200, 191),
-    (36, 149, 206), (20, 83, 104), (64, 66, 57), (157, 201, 221), (101, 141, 131), (131, 128, 121)]
-
-painter = Turtle()
-painter.shape("arrow")
-canvass.colormode(255)
-painter.penup()
+    (36, 149, 206), (20, 83, 104), (64, 66, 57), (157, 201, 221), (101, 141, 131), (131, 128, 121)
+]
 
 
 def hirst_painting(starting_x, starting_y):
     painter.setposition(starting_x, starting_y)
-    for row in range(0, 10):
+    for _ in range(0, 10):
         for row in range(0, 10):
             painter.dot(20, random.choice(color_list))
             painter.forward(50)
@@ -41,6 +42,6 @@ def hirst_painting(starting_x, starting_y):
         painter.seth(0)
 
 
-hirst_painting(-230, -215)
 painter.hideturtle()
+hirst_painting(-230, -215) #centers the screen window
 canvass.exitonclick()
