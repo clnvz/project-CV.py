d2e29284b414bd37beda11204ebd34bc1ab45bbb
clnvz <cybers.colinvelasquez@gmail.com>
2024-08-12 07:46:19 +0800

    Update main.py
    Changed output file type from .txt to .docx

--- Intermediate Projects/Day 24 - Mail Merge/Mail Merge Project Start/main.py
+++ Intermediate Projects/Day 24 - Mail Merge/Mail Merge Project Start/main.py
@@ -9,7 +9,7 @@ with open("./Input/Letters/starting_letter.txt", mode="r") as letter:
 template = template.replace("Angela", "Colin")
 
 for guest in guests:
-    with open(f"./Output/ReadyToSend/letter_for_{guest}.txt", mode="w") as draft:
+    with open(f"./Output/ReadyToSend/letter_for_{guest}.docx", mode="w") as draft:
         template = template.replace("[name]", guest)
         draft.write(template)
         template = template.replace(guest, "[name]")
